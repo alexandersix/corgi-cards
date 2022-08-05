da65ea8d12b1550c64fefa99e7bbccadcecdb3c4
Alexander Six <alexanderhsix@gmail.com>
2022-08-04 20:45:21 -0400

    Adds User seeder & data to User factory

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.co',
+            'password' => Hash::make('password'),
+            'currency' => 10000,
+        ]);
+
+        User::factory()->count(10)->create();
+    }
+}
