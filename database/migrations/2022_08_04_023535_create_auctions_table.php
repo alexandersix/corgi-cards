a93147583e3ce7036a57c21a55a02f618f6857fe
Alexander Six <alexanderhsix@gmail.com>
2022-08-04 20:45:37 -0400

    Adds end time column to Auction table

--- database/migrations/2022_08_04_023535_create_auctions_table.php
+++ database/migrations/2022_08_04_023535_create_auctions_table.php
@@ -2,6 +2,7 @@
 
 use App\Models\Card;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -24,6 +25,7 @@ return new class extends Migration
             $table->unsignedInteger('buyout_price')->nullable();
             $table->unsignedInteger('selling_price')->nullable();
             $table->dateTime('sold_at')->nullable();
+            $table->dateTime('ends_at')->default(Carbon::now()->addMinutes(5));
             $table->timestamps();
         });
     }
